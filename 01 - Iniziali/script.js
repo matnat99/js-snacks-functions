b2dc926c31e5fe7213d8d2608c733e7f8882f95f
mattianatale <mattianatale07@gmail.com>
2024-12-05 15:46:22 +0100

    feat: add exercise iniziali

---
+++ 01 - Iniziali/script.js
@@ -0,0 +1,23 @@
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+
+const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+
+
+// Dichiara la funzione qui.
+function getInitials(names){
+    let initials = []
+
+    for(i = 0; i <= 5; i++){
+        let firstIndex = names[i]
+        initials.push(firstIndex[0].toUpperCase())
+    }
+    return initials
+}
+
+
+// Invoca la funzione qui e stampa il risultato in console
+const initials = getInitials(names)
+console.log(initials)
+
+
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
